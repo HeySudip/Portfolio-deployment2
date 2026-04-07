5c055d80cc60a029d10ed7ac87e8383173ed98d9
Sudip Mahapatra <163993895+HeySudip@users.noreply.github.com>
2026-04-07 15:51:40 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
+  plugins: [react(), tailwindcss(), viteSingleFile()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  server: {
+    host: "0.0.0.0",
+    allowedHosts: ["4500-ibd145z6nb2i8m3cyv2ee.e2b.app", "all"],
+  },
+});
